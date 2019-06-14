c17f34052ff2194bc5911311fbf5e7be2a6abad2
Simon <liurunyuan0802@163.com>
2019-06-14 08:04:10 -0400

    first saving

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+ * main.c
+ */
+#include "DSP2833x_Device.h"     // DSP280x Headerfile Include File
+#include "DSP2833x_Examples.h"   // DSP280x Examples Include File
+#include <stdio.h>
+
+int fputc(int ch, FILE* stream)
+{
+
+//    while(SciaRegs.)
+    return ch;
+}
+
+
+int main(void)
+{
+    InitSysCtrl();
+
+    InitSciGpio();
+    printf("test\n");
+	return 0;
+}
